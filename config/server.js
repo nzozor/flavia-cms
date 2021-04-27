9449aaeb1c633303eb58fc9a080c47b749be63d1
nick.z <nick.zozor@onefile.co.uk>
2021-04-27 12:31:51 +0100

    port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd6375c783d627a984e5447245340dd6d'),
