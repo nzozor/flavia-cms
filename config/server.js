237663ffa143130ccae09b4b88c29e565454fa86
nick.z <nick.zozor@onefile.co.uk>
2021-05-27 18:23:07 +0100

    test ssl

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
+  url: 'https://cms.tracksdigital.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd6375c783d627a984e5447245340dd6d'),
