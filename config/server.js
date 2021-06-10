6327e3f33ec31b2bf865cb634fd61210cc5cb833
nick.z <nick.zozor@onefile.co.uk>
2021-06-10 12:09:19 +0100

    update db for page layout changes

--- config/server.js
+++ config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
-  url: 'https://cms.tracksdigital.com',
+  // url: 'https://cms.tracksdigital.com',
+  // url: 'http://192.168.1.203',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd6375c783d627a984e5447245340dd6d'),
